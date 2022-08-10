a9e39f649e25bd79f244f392dfa1a27970db264a
unknown <16521249@mahasiswa.itb.ac.id>
2022-08-10 21:17:53 +0700

    adding desc to check and about in help

--- src/help.js
+++ src/help.js
@@ -1,3 +1,6 @@
 module.exports = async function help(context){
-    await context.sendText("/about\n/check");
+    let str = `K17 Fact Checker Bot Help Menu
+    /check: ketik "/check <judul berita/artikel/fakta>" untuk mengecek rating atau klaim dari suatu berita/artikel/fakta.
+    /about: ketik "/about" untuk mengakses informasi dasar dari bot K17 Fact Checker Bot`
+    await context.sendText(str);
 }

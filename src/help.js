d6a78c41ea05507dcf17c9b5fbe3b7e6410693ad
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2022-08-14 21:41:05 +0700

    bolding text in help

--- src/help.js
+++ src/help.js
@@ -1,11 +1,11 @@
 module.exports = async function help(context){
     let str = `Untuk menggunakan Chatbot ini, kamu bisa mengirimkan salah satu dari ketiga command berikut:
-    1.  /check
+    1.  /𝗰𝗵𝗲𝗰𝗸
         Fitur check digunakan untuk mengecek kebenaran suatu klaim.
         Contoh: /check Virus Corona tidak berbahaya 
-    2.  /help
+    2.  /𝗵𝗲𝗹𝗽
         Fitur help berisi daftar fitur yang dapat digunakan.  
-    3.  /about
+    3.  /𝗮𝗯𝗼𝘂𝘁
         Fitur about berisi tentang kelompok milestone kami serta sumber data yang digunakan.`
     await context.sendText(str);
 }

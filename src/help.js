8be30f2be2fad9518613ddc8a65a462957abee23
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2022-08-14 16:51:12 +0700

    changing help indent

--- src/help.js
+++ src/help.js
@@ -1,8 +1,11 @@
 module.exports = async function help(context){
     let str = `Untuk menggunakan Chatbot ini, kamu bisa mengirimkan salah satu dari ketiga command berikut:
-    1. /check: Fitur check digunakan untuk mengecek kebenaran suatu klaim.
+    1.  /check
+        Fitur check digunakan untuk mengecek kebenaran suatu klaim.
         Contoh: /check Virus Corona tidak berbahaya 
-    2. /help: Fitur help berisi daftar fitur yang dapat digunakan.  
-    3. /about: Fitur about berisi tentang kelompok milestone kami serta sumber data yang digunakan.`
+    2.  /help
+        Fitur help berisi daftar fitur yang dapat digunakan.  
+    3.  /about
+        Fitur about berisi tentang kelompok milestone kami serta sumber data yang digunakan.`
     await context.sendText(str);
 }

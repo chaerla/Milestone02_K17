24a19dc83db5c4ca572853d77d3deec45d9a6c51
HobertJonatan <87473647+HobertJonatan@users.noreply.github.com>
2022-08-14 19:05:50 +0700

    menambahkan pesan error

--- src/error.js
+++ src/error.js
@@ -1,4 +1,4 @@
 module.exports = async function error(context){
-  let message = '<pesan error>';
+  let message = 'Maaf, untuk sekarang, kami belum bisa memproses command yang kamu berikan nih. Tapi tenang aja, kamu bisa gunakan fitur /help untuk melihat daftar command yang dapat digunakan!';
   await context.sendText(message);
 }

82e89783093a9fdde704f47fbffec0807646b7f9
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2022-08-14 18:34:20 +0700

    removing welcome to bottender

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const about = require ("./about.js")
 const error = require ("./error.js")
 
 module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
   if (context.event.text.trim().split(' ')[0] === '/check') {
     return withProps(check,{claim: context.event.text} );
   } else if (context.event.text.trim() === "/about") {
